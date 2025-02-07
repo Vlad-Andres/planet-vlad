a935cd3450f66ac13133bf5424f77e0581ba9032
vladmutruc <v.mutruc@creative-ct.nl>
2025-02-07 10:37:37 +0100

    Add config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command, mode }) => {
-    return {
-        resolve: {
-            alias: {
-                'babylonjs': mode === 'development' ? 'babylonjs/babylon.max' : 'babylonjs'
-            }
-        }
-    };
-});
+// export default defineConfig(({ command, mode }) => {
+//       // set base url for production to /vue3-todo/
+//     base: process.env.NODE_ENV === 'production' ? '/vue3-todo/' : '/',
+//     return {
+//         resolve: {
+//             alias: {
+//                 'babylonjs': mode === 'development' ? 'babylonjs/babylon.max' : 'babylonjs'
+//             }
+//         }
+//     };
+// });
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    // set base url for production to /vue3-todo/
+    base: process.env.NODE_ENV === 'production' ? '/vue3-todo/' : '/',
+  })
